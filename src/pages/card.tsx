db35f78869d3bbc2990daba9f0cc57a95cb33922
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-07-06 13:57:05 +0300

    feat: animated card

--- src/pages/card.tsx
+++ src/pages/card.tsx
@@ -1,42 +1,60 @@
 import Navbar from "@/utils/navbar";
-import image from "../assets/space.jpg"
-import Image from "next/image";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Card(){
+    const [show, setShow] = useState(true);
+
+    function unShow(){
+        setShow(false);
+    }
+
     return(
-        <div>
+        <div onClick={unShow}>
             <header>
                 <Navbar/>
             </header>
-            
-            <div className="blog">
-                <div className="title-box">
-                    <h3>
-                        Gorgeous card
-                    </h3>
-                    <hr/>
-                    <div className="intro">
-                        Framer motion
-                    </div>
-                </div>  
-                <div className="info">
-                    <span>
-                        Simple card to show how framer motion is sexy
-                    </span>
-                </div>
-                <div className="footer">
-                    <div className="icon-holder">
-                        <span>
-                            <i className="fa fa-comment-o"></i>
-                        <span>no ?</span>
-                &nbsp
-                            <i className="fa fa-calendar"></i>
-                            <span>06.07.2023</span>
-                        </span>
-                </div>
-            </div>
-            <div className="color-overlay"></div>
-            </div>
+
+            <AnimatePresence>
+                {show && (
+                    <motion.div
+                        exit={{
+                            y: -1000
+                        }} 
+                        transition={{
+                            duration: 1
+                        }}
+                        className="blog"
+                    >
+                        <div className="title-box">
+                            <h3>
+                                Gorgeous card
+                            </h3>
+                            <hr/>
+                            <div className="intro">
+                                Framer motion
+                            </div>
+                        </div>  
+                        <div className="info">
+                            <span>
+                                Simple card to show how framer motion is sexy
+                            </span>
+                        </div>
+                            <div className="footer">
+                                <div className="icon-holder">
+                                    <span>
+                                        <i className="fa fa-comment-o"></i>
+                                    <span>no ?</span>
+                            &nbsp
+                                        <i className="fa fa-calendar"></i>
+                                        <span>06.07.2023</span>
+                                    </span>
+                            </div>
+                        </div>
+                <div className="color-overlay"></div>
+                </motion.div>
+            )}
+            </AnimatePresence>
         </div>
     )
 }
