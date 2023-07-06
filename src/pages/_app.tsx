920b87d1afdd3944e7e267ad76c82ad9cbac1898
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-07-06 13:44:31 +0300

    feat: card

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import "../styles/navbar.css"
 import "../styles/main.css"
 import "../styles/button.css"
+import "../styles/card.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/card.tsx
+++ src/pages/card.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/utils/navbar";
+import image from "../assets/space.jpg"
+import Image from "next/image";
 
 export default function Card(){
     return(
@@ -6,7 +8,35 @@ export default function Card(){
             <header>
                 <Navbar/>
             </header>
-            <h3>Hello card</h3>
+            
+            <div className="blog">
+                <div className="title-box">
+                    <h3>
+                        Gorgeous card
+                    </h3>
+                    <hr/>
+                    <div className="intro">
+                        Framer motion
+                    </div>
+                </div>  
+                <div className="info">
+                    <span>
+                        Simple card to show how framer motion is sexy
+                    </span>
+                </div>
+                <div className="footer">
+                    <div className="icon-holder">
+                        <span>
+                            <i className="fa fa-comment-o"></i>
+                        <span>no ?</span>
+                &nbsp
+                            <i className="fa fa-calendar"></i>
+                            <span>06.07.2023</span>
+                        </span>
+                </div>
+            </div>
+            <div className="color-overlay"></div>
+            </div>
         </div>
     )
 }
