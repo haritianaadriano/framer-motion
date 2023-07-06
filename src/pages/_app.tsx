61d66e2d70980970a4570ca41c3118567817bcb7
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-07-06 12:23:43 +0300

    feat: button not animated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import "../styles/navbar.css"
 import "../styles/main.css"
+import "../styles/button.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/button.tsx
+++ src/pages/button.tsx
@@ -2,11 +2,14 @@ import Navbar from "@/utils/navbar";
 
 export default function Button(){
     return(
-        <div>
+        <div className="button-page-parent">
             <header>
                 <Navbar/>
             </header>
-            <h3>Hello Button</h3>
+            <h3>Awesome button</h3>
+            <div className="button-container">
+                <button className="button-19" role="button">Button</button>
+            </div>
         </div>
     )
 }
