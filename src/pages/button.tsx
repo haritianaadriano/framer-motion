03775aec94c8448ea0a282f3c74e0b92f67dd7c9
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-07-06 13:11:35 +0300

    feat: animated button and clicked animation

--- src/pages/button.tsx
+++ src/pages/button.tsx
@@ -1,14 +1,60 @@
 import Navbar from "@/utils/navbar";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function Button(){
+    const [show, setShow] = useState(false);
+
+    function handleClickShow() {
+        setShow(true);
+    }
+
+    function handleClickDontSchow(){
+        setShow(false);
+    }
+
     return(
         <div className="button-page-parent">
             <header>
                 <Navbar/>
             </header>
             <h3>Awesome button</h3>
+
             <div className="button-container">
-                <button className="button-19" role="button">Button</button>
+                <motion.button
+                    onClick={handleClickShow}
+                    whileHover={{
+                        opacity: 1.1,
+                        textShadow: "0px 0px 8px rgb(255, 255, 255)"
+                    }}
+                    transition={{
+                        duration: 3,
+                        type: "spring"
+                    }}
+                    initial={{
+                        y: -250
+                    }} 
+                    animate={{
+                        y: -10,
+                    }}
+                    className="button-19" 
+                    role="button"
+                >
+                    Click me
+                </motion.button>
+
+                {show && (
+                    <motion.h1
+                        initial={{
+                            y: 300
+                        }}
+                        animate={{
+                            y: -20
+                        }}
+                    >
+                        I'm beautiful no ?
+                    </motion.h1>
+                )}
             </div>
         </div>
     )
